e3e07f08a6caf7d178e3cb8e7be02cb568869bda
tinay <galvez.mcj@gmail.com>
2023-03-28 18:27:19 +0800

    added template file for all job postings page

--- app.py
+++ app.py
@@ -15,6 +15,13 @@ def index():
     return render_template('index.html', jobs=jobs)
 
 
+@app.route("/jobs")
+def job_postings():
+    jobs = load_jobs_from_db()
+    return render_template('allJobsPage.html', jobs=jobs)
+
+
+
 @app.route("/job/<id>")
 def show_job(id):
     job = load_job_from_db(id) 
